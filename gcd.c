00b3cdeda86a93fe867f00dd3d6e9f914d755a47
Jiffy953 <44827562+Jiffy953@users.noreply.github.com>
2020-02-07 18:09:15 -0500

    Update gcd.c

--- gcd.c
+++ gcd.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 int main(){
-    int n1, n2, d = 2;
+    int n1, n2, temp1, temp2;
     printf("Enter two postive integers: ");
     scanf("%d%d", &n1, &n2);
-    while (d <= n1 && d <= n){
-      d++;
-        if (n1 % d == 0 && n2 % d == 0){
-          printf("The GCD is: %d", d);
-    }
+    temp1 = n1;
+    temp2 = n2;
+    while (n1 != n2){
+        if (n1 > n2 )
+          n1 -= n2;
+        else
+          n2 -= n1;
   }
+  printf("The greatest common factor of %d and %d is: %d", temp1, temp2, n1);
 }
