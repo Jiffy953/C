ebfb24012a64155511078c479066ff84aee61cbb
Jiffy953 <44827562+Jiffy953@users.noreply.github.com>
2020-03-10 15:18:34 -0400

    commit

--- pow_base.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-int main()
-{
-  float base, power;
-  printf("Enter a base and then power: ");
-  scanf("%f%f", &base, &power);
-  printf("%.2f", pow(base, power));
-}
