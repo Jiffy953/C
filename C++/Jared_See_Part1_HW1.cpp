27a07494825937d9b3ad430f069bd7a6a393071e
Jared See <jareds2147@gmail.com>
2020-09-25 17:08:15 -0400

    zeep

--- C++/Jared_See_Part1_HW1.cpp
+++ C++/Jared_See_Part1_HW1.cpp
@@ -7,11 +7,28 @@ int main()
 {
     int x, y;
     cout << "Enter two values: ";
-    cin >> x >> y;
+    cin >> x;
+    cin >> y;
     cout << gcd(x,y);
 }
 
 int gcd(int x, int y)
 {
-    while()
+    if( (x - y) > 0 && x % y == 0)
+    {
+        return y;
+    }
+    if( (y - x) > 0 && y % x == 0)
+    {
+        return x;
+    }
+    if ( (x - y) > 0 && x % y != 0)
+    {
+        gcd(x - y, y);
+    }
+    if ( (y - x) > 0 && y % x != 0)
+    {
+        gcd(x, y - x);
+    }
+    
 }
