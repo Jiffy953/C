c8b63ba6a6798e02f8bf5668b7d3af1d3b32d5cd
Jared See <jareds2147@gmail.com>
2020-09-25 14:48:49 -0400

    zeep

---
+++ C++/Jared_See_Part1_HW1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+int gcd(int x, int y);
+int main()
+{
+    int x, y;
+    cout << "Enter two values: ";
+    cin >> x >> y;
+    cout << gcd(x,y);
+}
+
+int gcd(int x, int y)
+{
+    while()
+}
