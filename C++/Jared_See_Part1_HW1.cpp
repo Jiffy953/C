ac02b3e08d33e018f66a9ccbdb4eb81009e0e4b6
Jared See <jareds2147@gmail.com>
2020-09-26 14:12:52 -0400

    zeep

--- C++/Jared_See_Part1_HW1.cpp
+++ C++/Jared_See_Part1_HW1.cpp
@@ -1,31 +1,6 @@
 #include<iostream>
 using namespace std;
 
-<<<<<<< HEAD
-int gcd(int x, int y);
-int main()
-{
-    int x,y;
-    cin >> x;
-    cin >> y;
-    cout << gcd(x,y) << endl;
-}
-int gcd(int x, int y)
-{
-    {
-        if(x > y)
-        {
-            for(int i = y; i > 0 ; i--)
-            {
-                
-            }
-        }
-        else
-        {
-            gcd(x,y);
-        }
-    }
-=======
 
 int gcd(int x, int y);
 int main()
@@ -55,6 +30,4 @@ int gcd(int x, int y)
     {
         gcd(x, y - x);
     }
-    
->>>>>>> 27a07494825937d9b3ad430f069bd7a6a393071e
 }
